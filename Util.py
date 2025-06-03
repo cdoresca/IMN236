6ea30dbc5b000c909744aa776a78373084efa96d
Charles Savignac <savc1202@usherbrooke.ca>
2025-06-03 12:49:24 -0400

    Finished generating calibration points

--- Util.py
+++ Util.py
@@ -1,11 +1,19 @@
 import json
 import cv2 as cv
 import numpy as np
+from collections import defaultdict
+
+def Save_As_Json(array, file_name, path="./Points/"):
+
+    data = {"points": array}
+
+    with open(path+file_name, "w") as f:
+        json.dump(data, f, indent=3)
 
 
-def Generate_Base_Points():
+def Generate_Base_Points(file_name = "Base_Points_Grid.json"):
     points = []
-    point = 1
+    point = 0
 
     for i in range(16):
         for j in range(16):
@@ -17,23 +25,59 @@ def Generate_Base_Points():
             })
             point += 1
 
-    data = {"points": points}
-
-    with open("../Points/Base_Point_grid.json", "w") as f:
-        json.dump(data, f, indent=3)
+    Save_As_Json(points, file_name)
 
 def Generate_Img_Points(img):
-    
     img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img_gray = np.float32(img_gray)
 
-    brightness = 10 
-    contrast = 2.3  
+    img_gray = cv.normalize(img_gray, None, 0, 255, cv.NORM_MINMAX)
+    img_gray = cv.GaussianBlur(img_gray, (5, 5), 0)
 
-    img_gray = cv.addWeighted(img_gray, contrast, np.zeros(img_gray.shape, img_gray.dtype), 0, brightness)
+    sharpening_kernel = np.array([[0, -1, 0],
+                                  [-1, 5, -1],
+                                  [0, -1, 0]])
     
-    corners = cv.goodFeaturesToTrack(img_gray, maxCorners=250, qualityLevel=0.01, minDistance=10)
-    corners = np.int32(corners)
+    img_gray = cv.filter2D(img_gray, -1, sharpening_kernel)
 
+    brightness = 10 
+    contrast = 5
+    img_gray = cv.addWeighted(img_gray, contrast, np.zeros(img_gray.shape, img_gray.dtype), 0, brightness)
+
+
+    corners = cv.goodFeaturesToTrack(img_gray,
+                                     maxCorners=260,
+                                     qualityLevel=0.01,
+                                     minDistance=2,
+                                     mask=None,
+                                     useHarrisDetector=True,
+                                     k=0.04)
+
+    corners = cv.cornerSubPix(np.float32(img_gray),
+                              np.float32(corners).reshape(-1, 1, 2),
+                              winSize=(5, 5), zeroZone=(-1, -1),
+                              criteria=(cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 40, 0.01))
+    
+    corners = np.int32(corners)
     return corners
 
+def Remove_False_Corners(points, xThreshold = 0, yThreshold = 0):
+    index = 0
+    for x,y in points:
+        if x < xThreshold or y < yThreshold:
+            points.pop(index) 
+        index +=1
+
+def Sort_By_Columns(points, tolerence = 15):
+    columns = defaultdict(list)
+    for x, y in points:
+        group_x = round(x / tolerence) * tolerence
+        columns[group_x].append((x, y))
+
+    for group_x in columns:
+        columns[group_x].sort(key=lambda p: p[1])
+
+    sorted_by_columns = []
+    for group_x in sorted(columns.keys()):
+        sorted_by_columns.extend(columns[group_x])
+    return sorted_by_columns
--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import cv2 as cv
-import numpy as np
 import matplotlib.pyplot as plt
-from Util import Generate_Base_Points, Generate_Img_Points
+from Util import Generate_Base_Points, Generate_Img_Points, Remove_False_Corners, Sort_By_Columns, Save_As_Json
 
 #Generate_Base_Points()
 img = cv.imread('test.jpg')
 corners = Generate_Img_Points(img)
 
-for corner in corners:
-    x, y = corner.ravel()
-    cv.circle(img, (x, y), 3, (0, 0, 255), -1)
+points = [tuple(pt.ravel()) for pt in corners]
+Remove_False_Corners(points, 650, 150)
+sorted = Sort_By_Columns(points)
 
-img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+array = []
+index = 0
+for x, y in sorted:
+    array.append({
+        "point": index,
+        "X": int(x),
+        "Y": int(y),
+        "Z": 0
+    })
+    index += 1
+Save_As_Json(array, "Test_Calibration_Grid.json")
 
-plt.imshow(img_rgb)
+for idx, (x, y) in enumerate(sorted):
+    cv.circle(img, (int(x), int(y)), 3, (0, 0, 255), -1)
+    cv.putText(img, str(idx), (int(x) + 5, int(y) - 5), cv.FONT_HERSHEY_SIMPLEX, 
+               0.5, (0, 255, 0), 1, cv.LINE_AA)
+    
+
+plt.imshow(img)
 plt.axis('off')
 plt.show()
