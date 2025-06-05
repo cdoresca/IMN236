41059e366891122614cefe5c5654cfff9bcd1c92
Charles Savignac <savc1202@usherbrooke.ca>
2025-06-05 15:33:41 -0400

    main change

--- main.py
+++ main.py
@@ -1,264 +1,189 @@
 import cv2 as cv
-import numpy as np
 import matplotlib.pyplot as plt
-import sys
-from scipy.spatial.transform import Rotation as R_scipy
-from Util import Open_Json
+import numpy as np
+import Util as ut
+
+def corners(img):
+    corners = ut.Generate_Img_Points(img)
+
+    points = [tuple(pt.ravel()) for pt in corners]
+    ut.Remove_False_Corners(points, 650, 150)
+    sorted = ut.Sort_By_Columns(points)
+
+    array = []
+    index = 0
+    for x, y in sorted:
+        array.append({
+            "point": index,
+            "X": int(x),
+            "Y": int(y),
+            "Z": 0
+        })
+        index += 1
+    ut.Save_As_Json(array, "Test_Calibration_Grid.json")
 
-# === PARAMÈTRES CAMÉRA ===
-Zprime = 4       # Focale en mm
-Sx = Sy = 0.0014 # Taille pixel en mm
-Om, On = 1250, 750  # Centre image
-
-# === MATRICE INTRINSÈQUE ===
-Mint = np.array([
-    [Zprime / Sx, 0, Om],
-    [0, Zprime / Sy, On],
-    [0, 0, 1]
-])
-
-# === CHARGEMENT DES POINTS ===
-data_original = Open_Json("Base_Points_Grid.json")
-data_calibration = Open_Json("Test_Calibration_Grid.json")
-
-points_objet = np.array([[pt["X"], pt["Y"], pt["Z"]] for pt in data_original["points"]], dtype=np.int64)
-points_monde = np.array([[pt["X"], pt["Y"]] for pt in data_calibration["points"]], dtype=np.int64)
-
-# === TRANSFORMATION EN COORDONNÉES EN MM ===
-Xdi = (points_monde[:, 0] - Om) * Sx
-Ydi = (points_monde[:, 1] - On) * Sy
-
-# === ÉTAPE 0 : Résolution des coefficients ===
-indices = [0, 5, 48, 63, 112]
-A = []
-b = []
-
-for i in indices:
-    X, Y = points_objet[i][:2]
-    xdi, ydi = Xdi[i], Ydi[i]
-    A.append([ydi * X, ydi * Y, ydi, -xdi * X, -xdi * Y])
-    b.append([xdi])
-
-A = np.array(A)
-b = np.array(b)
-x = np.linalg.solve(A, b)
-
-r11, r12, Tx, r21, r22 = x.flatten()
-
-# === ÉTAPE 2 : Calcul de Ty ===
-Sr = r11**2 + r12**2 + r21**2 + r22**2
-det = r11 * r22 - r12 * r21
-
-if det != 0:
-    Ty = (Sr - np.sqrt(Sr**2 - 4 * det**2)) / (2 * det)
-else:
-    Ty = 1 / (r11**2 + r21**2 or r12**2 + r22**2)
-
-Ty = abs(Ty)  # Forcer signe positif, inversé plus tard si nécessaire
-
-# === ÉTAPE 3 : Matrice de rotation + Tx ===
-R11, R12 = r11 * Ty, r12 * Ty
-R21, R22 = r21 * Ty, r22 * Ty
-Tx *= Ty
-
-# Vérification du signe de Ty
-Xs, Ys, _ = points_objet[0]
-x_tild = R11 * Xs + R12 * Ys + Tx
-y_tild = R21 * Xs + R22 * Ys + Ty
-
-if not (np.sign(x_tild) == np.sign(Xdi[0]) and np.sign(y_tild) == np.sign(Ydi[0])):
-    Ty = -Ty
-    R11, R12, R21, R22, Tx = -R11, -R12, -R21, -R22, -Tx
-
-# Matrice de rotation 3x3
-L1 = np.array([R11, R12, np.sqrt(1 - R11**2 - R12**2)])
-L2 = np.array([R21, R22, np.sqrt(1 - R21**2 - R22**2)])
-L3 = np.cross(L1, L2)
-R = np.vstack([L1, L2, L3])
-
-# === ÉTAPE 5 : Estimation Zprime et Tz ===
-A = []
-b = []
-
-for i in [48, 112]:
-    X, Y = points_objet[i][:2]
-    Y_val = R[1, 0]*X + R[1, 1]*Y + Ty
-    W_val = R[2, 0]*X + R[2, 1]*Y
-
-    A.append([Y_val, -Ydi[i]])
-    b.append([W_val * Ydi[i]])
-
-x = np.linalg.solve(np.array(A), np.array(b))
-Tz, Zprime_est = x.flatten()
-
-T = np.array([[Tx], [Ty], [Tz]])
-
-
-def Calculer_erreur(params):
-    rvec = params[0:3]
-    tvec = params[3:6]
-    Zprime = params[6]
-    k1 = params[7]
-
-    R_mat = R_scipy.from_rotvec(rvec).as_matrix()
-
-    erreurs = []
-    for i in range(len(points_objet)):
-        X, Y, Z = points_objet[i]
-        pt3D = np.array([X, Y, Z])
-
-        X_cam = R_mat @ pt3D + tvec
-        x = X_cam[0]
-        y = X_cam[1]
-        w = X_cam[2]
-
-        xd = Zprime * x / w
-        yd = Zprime * y / w
-        r2 = xd**2 + yd**2
-        xd_corr = xd * (1 + k1 * r2)
-        yd_corr = yd * (1 + k1 * r2)
-
-        u = xd_corr / Sx + Om
-        v = yd_corr / Sy + On
-
-        u_meas, v_meas = points_monde[i]
-        erreurs.extend([u - u_meas, v - v_meas])
-
-    return np.array(erreurs) 
-
-def Calcule_Jacobien(params):
-    n_params = len(params)
-
-    e0 = Calculer_erreur(params)
-    J = np.zeros((len(e0), n_params))
-
-    for j in range(n_params):
-        delta = np.zeros_like(params)
-        delta[j] = 0.000001
-        el = Calculer_erreur(params + delta)
-        J[:, j] = (el -e0)/0.000001
-    return J
-
-
-def Levenber_Marquardt(params, kmax):
-    k = 0
-    v = 2
-    p = params
-
-    J = Calcule_Jacobien(params)
-    e = Calculer_erreur(params)
-    Jprime = np.transpose(J)
-
-    A = Jprime * J
-    g = Jprime * e
-
-    trouve = max(np.linalg.norm(g)) <= sys.float_info.epsilon
-    τ = 2 * np.pi
-    μ = τ * max(np.diag(A))
-
-    while not trouve or k< kmax:
-        k = k+1
-        h = (A + μ * np.identity(n))**-1*(-g)
-        if(np.linalg.norm(h) <= sys.float_info.epsilon * np.linalg.norm(p)):
-            trouve = True
-        else:
-            Pnew = p + h
-            pp = (Calculer_erreur(p) - Calculer_erreur(Pnew)) / (np.transpose(h) * (μ * h - g))
-
-            if pp > 0:
-                p = Pnew
-                J = Calcule_Jacobien(p)
-                Jprime = np.transpose(J)
-                e = Calculer_erreur(p)
-
-                A = Jprime*J
-                g = Jprime*e
-            
-                trouve = max(np.linalg.norm(g)) <= sys.float_info.epsilon
-
-                μ = μ * max(1/3, 1 - (2*pp - 1)**3)
-                v = 2
-            else:
-                μ = v * μ
-                v = 2 * v
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# === ÉTAPE 6 : Correction radiale (k1) ===
-xd_list = []
-yd_list = []
-r2_list = []
-dx_list = []
-dy_list = []
-
-A = []
-b = []
-
-for i in range(len(points_objet)):
-    X, Y, Z = points_objet[i]
-
-    xd = Zprime_est * (R[0] @ [X, Y, Z] + T[0]) / (R[2] @ [X, Y, Z] + T[2])
-    yd = Zprime_est * (R[1] @ [X, Y, Z] + T[1]) / (R[2] @ [X, Y, Z] + T[2])
-    r2 = xd**2 + yd**2
-
-    xd_list.append(xd)
-    yd_list.append(yd)
-    r2_list.append(r2)
-    dx = Xdi[i] - xd
-    dy = Ydi[i] - yd
-
-    A.append([xd * r2])
-    A.append([yd * r2])
-    b.append([dx])
-    b.append([dy])
-
-A = np.array(A)
-b = np.array(b)
-
-k1 = np.linalg.solve(A, b)[0, 0]
-
-# === ÉTAPE 7 : Reprojection des points corrigés ===
-proj_pts = []
-
-for i in range(len(points_objet)):
-
-    xd_corr = xd_list[i] * (1 + k1 * r2_list[i])
-    yd_corr = yd_list[i] * (1 + k1 * r2_list[i])
-
-    m = xd_corr / Sx + Om
-    n = yd_corr / Sy + On
-
-    proj_pts.append([m, n])
-
-proj_pts = np.array(proj_pts)
-
-# === AFFICHAGE DES POINTS REPROJETÉS ===
 img = cv.imread('test.jpg')
-plt.figure(figsize=(12, 9))
-plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
-plt.scatter(proj_pts[:, 0], proj_pts[:, 1], c='red', marker='x', s=20, label='Reprojetés')
-plt.legend()
+
+#Generate_Base_Points()
+#corners(img)
+
+z_prime = 26.0  # focal length in mm
+Sx = 0.0014    # pixel size x in mm/pixel
+Sy = 0.0014     # pixel size y in mm/pixel
+Om = 2000/2       # principal point x in pixels
+On = 1500/2     # principal point y in pixels
+
+Mint = np.array([[z_prime / Sx, 0, Om],
+                 [0, z_prime / Sy, On],
+                 [0,  0,  1]])
+
+R = np.eye(3)
+T = np.zeros((3, 1))
+
+
+
+data_original = ut.Open_Json("Base_Points_Grid.json")
+data_calibration = ut.Open_Json("Test_Calibration_Grid.json")
+
+points_original = data_original["points"]
+points_calibration = data_calibration["points"]
+
+pts_orig = np.array([[pt["X"], pt["Y"]] for pt in points_original], dtype=np.float32)
+pts_calib = np.array([[pt["X"], pt["Y"]] for pt in points_calibration], dtype=np.float32)
+
+x_dist=(pts_calib[:,0]-Om)*Sx
+y_dist=(pts_calib[:,1]-On)*Sy
+
+A=[]
+b=[]
+
+#index = np.random.randint(256,size=2) 
+index=[0,5,48,63,112]
+
+for i in index:
+    A.append([y_dist[i]*pts_orig[i][0],y_dist[i]*pts_orig[i][1],y_dist[i],-x_dist[i]*pts_orig[i][0],-x_dist[i]*pts_orig[i][1]])
+    b.append(x_dist[i])
+
+A = np.array(A)
+b = np.array(b)
+
+
+P = np.linalg.pinv(A) @ b
+
+
+
+S= P[0]**2+P[1]**2+P[3]**2+P[4]**2
+
+Ty=0
+
+if P[0]*P[4]-P[1]*P[3]!=0:
+    Ty=(S-np.sqrt(S**2-4*(P[0]*P[4]-P[1]*P[3])**2))/(2*(P[0]*P[4]-P[1]*P[3])**2)
+elif P[0]**2+P[3]**2!=0:
+    Ty=1/(P[0]**2+P[3]**2)
+    
+elif P[1]**2+P[4]**2!=0:
+    Ty=1/(P[1]**2+P[4]**2)
+    
+elif P[0]**2+P[1]**2!=0:
+    Ty=1/(P[0]**2+P[1]**2)
+elif P[3]**2+P[4]**2!=0:
+    Ty=1/(P[3]**2+P[4]**2)
+
+
+Ty=np.sqrt(Ty)
+
+P=P*Ty
+x_sign=P[0]*pts_orig[0][0]+P[1]*pts_orig[0][1]+P[2]
+y_sign=P[3]*pts_orig[0][0]+P[4]*pts_orig[0][1]+Ty
+
+if np.sign(x_sign)==np.sign(x_dist[0]) and  np.sign(y_sign)==np.sign(y_dist[0]):
+    Ty=Ty
+else:
+    Ty*=-1
+    P*=-1
+
+
+r13=np.sqrt(1-P[0]**2-P[1]**2)
+
+
+S=-1*np.sign(P[0]+P[1]+P[3]+P[4])
+
+r23=S*np.sqrt(1-P[3]**2-P[4]**2)
+
+L1=np.array([P[0],P[1],r13])
+L2=np.array([P[3],P[4],r23])
+L3=np.cross(L1,L2)
+
+
+Tx=P[2]
+R=np.array([L1,L2,L3])
+
+A=[]
+b=[]
+
+#index = np.random.randint(256,size=2) 
+index=[48,112]
+for i in index:
+    Y=R[1][0]*pts_orig[i][0]+R[1][1]*pts_orig[i][1]+Ty
+    W=R[2][0]*pts_orig[i][0]+R[2][1]*pts_orig[i][1]
+
+    A.append([Y, -y_dist[i]])
+    b.append([W*y_dist[i]])
+
+A=np.array(A)
+b=np.array(b)
+X=np.linalg.pinv(A) @ b
+
+T = np.array([Tx,Ty])
+
+r = x_dist**2 + y_dist**2
+params = np.array([0,X[0].item(),X[1].item()])
+
+denom = (R[2][0]*pts_orig[:, 0] + R[2][1]*pts_orig[:, 1] + params[2])
+
+
+
+m = (params[1]/Sx)*((R[0][0]*pts_orig[:, 0] + R[0][1]*pts_orig[:, 1] + Tx) / denom)  +Om
+n = (params[1]/Sy)*((R[1][0]*pts_orig[:, 0] + R[1][1]*pts_orig[:, 1] + Ty) / denom)  +On
+
+plt.imshow(img)
+
+plt.plot(m, n, 'ro')  # 'ro' = red + round marker
+plt.title("Points sur l'image")
+plt.axis('off')
 plt.show()
+
+params = ut.Levenberg_Marquardt(params,R,T,pts_orig,x_dist,r)
+
+radiale = (1 + params[0] * r)
+denom = (R[2][0]*pts_orig[:, 0] + R[2][1]*pts_orig[:, 1] + params[2])
+
+m = (params[1]/Sx) * (((R[0][0]*pts_orig[:, 0] + R[0][1]*pts_orig[:, 1] + Tx) / denom) * radiale) + Om 
+n = (params[1]/Sy) * (((R[1][0]*pts_orig[:, 0] + R[1][1]*pts_orig[:, 1] + Ty) / denom) * radiale) + On 
+
+plt.imshow(img)
+
+error = np.sqrt(np.mean((m - pts_calib[:, 0])**2 + (n - pts_calib[:, 1])**2))
+print(f"Erreur de reprojection RMS : {error:.2f} pixels")
+
+plt.plot(m, n, 'ro')  # 'ro' = red + round marker
+plt.title("Points sur l'image")
+plt.axis('off')
+plt.show()
+
+
+
+
+
+
+
+#================================================================================================================================
+#for idx, (x, y) in enumerate(sorted):
+#    cv.circle(img, (int(x), int(y)), 3, (0, 0, 255), -1)
+#    cv.putText(img, str(idx), (int(x) + 5, int(y) - 5), cv.FONT_HERSHEY_SIMPLEX, 
+#               0.5, (0, 255, 0), 1, cv.LINE_AA)
+#    
+#
+#plt.imshow(img)
+#plt.axis('off')
+#plt.show()
